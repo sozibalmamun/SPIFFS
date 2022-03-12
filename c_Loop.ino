a041f33177d5ad897868293c4b7d0a9eac20e015
Sozib Al Mamun <85221131+sozibalmamun@users.noreply.github.com>
2022-03-12 18:04:27 +0600

    Update c_Loop.ino

--- c_Loop.ino
+++ c_Loop.ino
@@ -1,11 +1,13 @@
 void loop()
 {
+  fileCount();
   Read("/data_one.txt", read_data_one);
   delay(2000);
   Read("/data_two.txt", read_data_two);
   delay(3000);
-//if you want to delete data uncomment bellow
-//  deleteFile("/data_one.txt");
-//  delay(2000);
-//  deleteFile("/data_two.txt");
+  //if you want to delete data uncomment bellow
+  //  deleteFile("/data_one.txt");
+  //  delay(2000);
+  //  deleteFile("/data_two.txt");
+  format();
 }
